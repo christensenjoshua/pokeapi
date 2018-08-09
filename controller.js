4e4817a7c0f7c8728c9085dea05661a462c577e9
christensenjoshua <christensen.joshua@outlook.com>
2018-08-08 20:42:38 -0600

    added sprites!

--- controller.js
+++ controller.js
@@ -1,22 +1,23 @@
-function Controller(){
+function Controller() {
     let service = new Service()
-    
-    function drawPokemon(){
+
+    function drawPokemon() {
         let template = ``
         let pokemon = service.accessPokemon()
-        for(let i = 0; i < pokemon.length; i++){
+        for (let i = 0; i < pokemon.length; i++) {
             const anPokemon = pokemon[i]
             console.log(anPokemon)
             let stats = `
             <h4>Stats</h4>
             `
-            for(let ii = 0; ii < anPokemon.myDetail.stats.length; ii++){
-                stats +=`
+            for (let ii = 0; ii < anPokemon.myDetail.stats.length; ii++) {
+                stats += `
                 <p>${anPokemon.myDetail.stats[ii].stat.name}: ${anPokemon.myDetail.stats[ii].base_stat}</p>
                 `
             }
             template += `
-            <div class="col-12 col-md-6 col-lg-4">
+            <div class="col-12 col-md-6 col-lg-4 text-center">
+                <img src="${anPokemon.myDetail.sprites.front_default}" />
                 <h3>${anPokemon.name}</h3>
                 ${stats}
             </div>
@@ -26,7 +27,7 @@ function Controller(){
     }
 
 
-    this.getPokemon = function(){
+    this.getPokemon = function () {
         service.getPokemon(drawPokemon)
     }
 }
